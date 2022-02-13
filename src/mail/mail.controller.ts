e6c17afc54a52015d18703e4986afc7c8e018824
Ardrit Krasniqi <ardritdev@gmail.com>
2022-02-13 23:05:39 +0100

    added the logics for sending confirmation email

--- src/mail/mail.controller.ts
+++ src/mail/mail.controller.ts
@@ -1,4 +1,16 @@
 import { Controller } from '@nestjs/common';
+import { EventPattern } from '@nestjs/microservices';
+import { MailService } from './mail.service';
 
-@Controller('mail')
-export class MailController {}
+@Controller()
+export class MailController {
+    constructor(private readonly mailService: MailService){}
+
+    // this microservice listents for cmd commands  
+
+
+    @EventPattern({cmd: "send-registration-email"})
+    async sendRegistrationEmail(email: string, confirmUrl: string){
+        return this.mailService.sendRegistrationEmail(email, confirmUrl);
+    }
+}
